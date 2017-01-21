e79347c273a57ab331a941879f1ea7ec570c9eb4
Aarni Koskela <akx@iki.fi>
2017-01-21 14:54:30 +0200

    Add test for POS view

---
+++ lippukala/tests/test_pos.py
@@ -0,0 +1,14 @@
+from lippukala.models import Code
+from lippukala.tests.utils import _create_test_order
+
+
+def test_pos_view(admin_client):
+    for x in range(5):
+        _create_test_order()
+    admin_client.get('/pos/')
+    codes = admin_client.get('/pos/', {'json': '1'}).json()['codes']
+    code_id = codes[0]['id']
+    admin_client.post('/pos/', {'use': code_id, 'station': 'hurp'})
+    code = Code.objects.get(pk=code_id)
+    assert code.is_used
+    assert code.used_at == 'hurp'
