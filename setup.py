86a4e83929d4d104ef25a89068d9476f8b4569db
Aarni Koskela <akx@iki.fi>
2021-06-21 18:53:30 +0300

    Become 2.0.0 (major dependency version incompatibilities)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='lippukala',
-    version='1.2.1',
+    version='2.0.0',
     description='Desuconin ja Traconin e-lippu-jarjestelma',
     author='Aarni Koskela',
     author_email='akx@desucon.fi',
